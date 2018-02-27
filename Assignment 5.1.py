3de775bb9a9655f28cf65e0d5ed2786fd9d999bb
shikhavaidh <36124149+shikhavaidh@users.noreply.github.com>
2018-02-27 22:29:20 +0530

    Assignment 5.1
    Assignment 5.1 and output

---
+++ Assignment 5.1.py
@@ -0,0 +1,49 @@
+
+# coding: utf-8
+
+# In[20]:
+
+#Assignment 5.1
+#Write a function to compute 5/0 and use try/except to catch the exceptions.
+
+(a,b) = (5,0)
+try:# simple use of try-except block for handling errors
+    g = a/b
+except ZeroDivisionError:
+    print ("This is a DIVIDED BY ZERO error")
+
+
+# In[ ]:
+
+
+
+
+# In[ ]:
+
+
+
+
+# In[ ]:
+
+
+
+
+# In[ ]:
+
+
+
+
+# In[ ]:
+
+
+
+
+# In[ ]:
+
+
+
+
+# In[ ]:
+
+
+
